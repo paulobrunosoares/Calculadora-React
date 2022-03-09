6495c21d1f99a100f02e3b2e5c2f39f00cafc5f0
PauloBSS <pbss2007@gmail.com>
2022-03-08 23:01:28 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Calc React</h1>
+        <h1>Calculadora em React</h1>
         <Calculadora />
         <div className="footer">
           <span><strong>℗</strong>Direitos reservados!<br/>Desenvolvido por: Paulo Bruno Soares</span>
