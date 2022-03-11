d0e93752c24018d0da65fcf261c8fab7be3fd610
PauloBSS <pbss2007@gmail.com>
2022-03-11 03:02:50 -0300

    style-calculadora

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./styles.css";
 import Calculadora from "./components/calc/Calculadora";
+import logoReact from "./assets/react.png"
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1>Calculadora em React</h1>
-        <Calculadora />
+
+        <div className="header"> 
+          <h1>Calculadora em React</h1>
+          <img src={logoReact} alt="logo" /> 
+        </div>
+
+        <div className="container">
+          <Calculadora />
+        </div>
         <div className="footer">
           <span><strong>℗</strong>Direitos reservados!<br/>Desenvolvido por: Paulo Bruno Soares</span>
         </div>
-      </div>
+      
     </div>
   );
 }
